22b7aed7dbcb01263b7ae023eedb877e3d18bb6e
Zhihaosh <shanzhihaoszh@gmail.com>
2017-09-21 11:28:30 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -3,6 +3,14 @@ var map_manager = {
     "map" : null,
     "map_items" : []
 }
+map_manager.map_items = [
+    {
+      "pokemon_id" : 12,
+      "expire" : 1506021588,
+      "longitude" : -73.9800345,
+      "latitude" : 40.7596651,
+    }
+]
 
 var map;
 function loadMapScenario() {
@@ -14,14 +22,7 @@ function loadMapScenario() {
 
 
 //1. define pokemon data format create pokemon data
-map_manager.map_items = [
-    {
-      "pokemon_id" : 12,
-      "expire" : 1506021588,
-      "longitude" : -73.9800345,
-      "latitude" : 40.7596651,
-    }
-]
+
 function get_counter_down_time_from_expire_epoch(epoch){
     var now_time = new Date().getTime()/1000;   
     var time_left = epoch - now_time;
@@ -46,7 +47,7 @@ function get_pokemon_layer_from_map_items(map_items) {
     return layer;
 }
 function add_pokemon_layer(){
-    var pokemon_layer = get_pokemon_layer_from_map_items(map_items)
+    var pokemon_layer = get_pokemon_layer_from_map_items(map_manager.map_items)
     map.layers.insert(pokemon_layer)
 }
 
