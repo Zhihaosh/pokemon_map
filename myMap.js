c3d6b9f987d29ef587da92802142fed8a36a335a
Zhihaosh <shanzhihaoszh@gmail.com>
2017-09-21 10:58:19 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,34 @@
 var map;
 function loadMapScenario() {
     map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Your Bing Maps Key'
+        credentials: 'AiapA0N7GG69bA5HrgU1nDW_6tNIO7eELxRVdgecgfF7mRVeGPsAqon929HNsYvh'
     });
 }
+
+
+//1. define pokemon data format create pokemon data
+map_manager.map_items = [
+    {
+      "pokemon_id" : 12,
+      "expire" : 1476589403,
+      "longitude" : -73.9800345,
+      "latitude" : 40.7596651,
+    }
+]
+
+//2. Create pokemon image on map
+function get_pokemon_layer_from_map_items(map_items) {
+    var layer = new Microsoft.Maps.Layer();
+    return layer;
+}
+var pokemon_layer = get_pokemon_layer_from_map_items(map_items)
+map.layers.insert(pokemon_layer)
+//3. Add pokemon counter down refresh
+
+
+//4. Connect with REST API
+
+
+ var map;
+                
+                
