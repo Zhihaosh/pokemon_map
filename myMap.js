773477412e9e4b25cf09fddd647849ab6fbd6d4d
Zhihaosh <shanzhihaoszh@gmail.com>
2017-09-21 11:35:41 -0700

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,16 +1,17 @@
-
 var map_manager = {
     "map" : null,
     "map_items" : []
 }
+
 map_manager.map_items = [
     {
       "pokemon_id" : 12,
-      "expire" : 1506021588,
+      "expire" : 1476589403,
       "longitude" : -73.9800345,
       "latitude" : 40.7596651,
     }
 ]
+
 function loadMapScenario() {
     map_manager.map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AikCEUj_rGAWu8x2XBM0lIgs25BcQN7AVFa7vTvQbpsVSMbcdTZB_7XgZJlR0t6g'
@@ -18,19 +19,16 @@ function loadMapScenario() {
     add_pokemon_layer();
 }
 
-
-//1. define pokemon data format create pokemon data
-
-function get_counter_down_time_from_expire_epoch(epoch){
-    var now_time = new Date().getTime()/1000;   
-    var time_left = epoch - now_time;
-    var sec = Math.floor(time_left % 60);
-    var min = Math.floor(time_left / 60);
-    return min + ":" + sec;
+// 1. Define pokemon data format, create mock pokemon data
+function get_counter_down_time_from_expire_epoch(epoch) {
+  var now_time = new Date().getTime() / 1000;
+  var time_left = epoch / 1000 - now_time;   // unit: second
+  var second = Math.floor(time_left % 60);
+  var minute = Math.floor(time_left / 60);
+  return minute + ":" + second;
 }
 
-
-//2. Create pokemon image on map
+// 2. Create pokemon image on map
 function get_pokemon_layer_from_map_items(map_items) {
     var layer = new Microsoft.Maps.Layer();
     var pushpins = []
@@ -49,7 +47,7 @@ function add_pokemon_layer() {
     map_manager.map.layers.insert(pokemon_layer);
 }
 
-//3. Add pokemon counter down refresh
+// 3. Add pokemon counter down refresh.
 function refresh_pokemon_layer() {
   // Prepare new layer
   var pokemon_layer = get_pokemon_layer_from_map_items(map_manager.map_items)
